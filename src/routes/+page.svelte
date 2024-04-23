5c3c36dcb7b49ec58813dd818ca08c5ec6f974a3
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-04-23 19:33:44 +0900

    Sticky location input layout: add to bottom.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,24 @@
 </script>
 
 <div class="container">
-	<pre>data = {JSON.stringify(data, null, 4)}</pre>
+	<div class="scroll">
+		<pre>data = {JSON.stringify(data, null, 4)}</pre>
+	</div>
+	<div>
+		<input type="text" value={`${data.location.city}, ${data.location.country}`} />
+	</div>
 </div>
+
+<style>
+	.container {
+		height: 100vh;
+		height: 100dvh;
+
+		display: grid;
+		grid-template-rows: 1fr auto;
+	}
+
+	.scroll {
+		overflow: auto;
+	}
+</style>

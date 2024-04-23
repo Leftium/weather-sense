9e591d4cc3fe8a2b3ab6103ccb5919ccc978f90a
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-04-24 02:57:19 +0900

    Geolocation: add.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,58 @@
 <script lang="ts">
+	import 'iconify-icon';
+
 	let { data } = $props();
+
+	let location: Record<string, any> = $state({
+		latitude: data.location.latitude,
+		longitude: data.location.longitude
+	});
+
+	function onGeolocate() {
+		const options = {
+			enableHighAccuracy: true
+		};
+
+		function success(pos: GeolocationPosition) {
+			const crd = pos.coords;
+
+			location = {
+				latitude: crd.latitude,
+				longitude: crd.longitude,
+				accuracy: crd.accuracy,
+				source: 'device geolocation'
+			};
+
+			console.log('Your current position is:');
+			console.log(`Latitude : ${crd.latitude}`);
+			console.log(`Longitude: ${crd.longitude}`);
+			console.log(`More or less ${crd.accuracy} meters.`);
+			console.log(location);
+		}
+
+		function error(err: GeolocationPositionError) {
+			location = {
+				error: err
+			};
+			console.warn(`ERROR(${err.code}): ${err.message}`);
+		}
+
+		navigator.geolocation.getCurrentPosition(success, error, options);
+	}
 </script>
 
 <div class="container">
 	<div class="scroll">
 		<pre>data = {JSON.stringify(data, null, 4)}</pre>
+		<pre>location = {JSON.stringify(location, null, 4)}</pre>
 	</div>
+
 	<div>
-		<input type="text" value={`${data.location.city}, ${data.location.country}`} />
+		<div role="group">
+			<button onclick={onGeolocate}><iconify-icon icon="gis:location-arrow"></iconify-icon></button>
+			<input type="text" value={`${location.latitude}, ${location.longitude}`} />
+			<button>Search</button>
+		</div>
 	</div>
 </div>
 

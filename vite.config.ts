95fcdec60be9e97cff1ba71f640c1a70f01e3b4b
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-04-23 15:35:43 +0900

    PicoCSS: add.
    `npx svelte-add@latest picocss`

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@use "src/variables.scss" as *;'
+			}
+		}
+	}
 });

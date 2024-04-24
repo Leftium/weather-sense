b49ee421c7c41efbb578fddd9e8d60122df62165
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-04-25 06:02:22 +0900

    Leaflet map: add.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import 'iconify-icon';
+	import { onMount, onDestroy } from 'svelte';
 	import { type WeatherDataEvents, makeNsWeatherData } from '$lib/ns-weather-data.svelte.js';
+	import { gg } from '$lib/gg.js';
 	import { getEmitter } from '$lib/emitter.js';
+
+	let mapElement: HTMLDivElement;
+	let leafletRemover: { remove: () => void };
 
 	let { data } = $props();
 
@@ -32,15 +37,39 @@
 
 		navigator.geolocation.getCurrentPosition(success, error, options);
 	}
+
+	onMount(async () => {
+		const L = await import('leaflet');
+
+		const lat = nsWeatherData.coords?.latitude || 0;
+		const lon = nsWeatherData.coords?.longitude || 0;
+
+		let map = (leafletRemover = L.map(mapElement).setView([lat, lon], 13));
+
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		}).addTo(map);
+	});
+
+	onDestroy(() => {
+		if (leafletRemover) {
+			gg('Unloading Leaflet map.');
+			leafletRemover.remove();
+		}
+	});
 </script>
 
 <div class="container">
 	<div class="scroll">
-		<pre>nsWeatherData = {JSON.stringify(nsWeatherData, null, 4)}</pre>
-		<pre>data = {JSON.stringify(data, null, 4)}</pre>
+		<div class="map" bind:this={mapElement}></div>
+
+		<div class="pico">
+			<pre>nsWeatherData = {JSON.stringify(nsWeatherData, null, 4)}</pre>
+			<pre>data = {JSON.stringify(data, null, 4)}</pre>
+		</div>
 	</div>
 
-	<div>
+	<div class="pico">
 		<div role="group">
 			<button onclick={onGeolocate}><iconify-icon icon="gis:location-arrow"></iconify-icon></button>
 			<input type="text" value={`${nsWeatherData.name}`} />
@@ -50,6 +79,10 @@
 </div>
 
 <style>
+	.map {
+		height: 600px;
+	}
+
 	.container {
 		height: 100vh;
 		height: 100dvh;

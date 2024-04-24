38fc38db020c2f788c24e7de9a7fba4245d9a24b
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-04-25 02:47:15 +0900

    Use actual lat/lng from vercel headers.
    Also adjusted accuracies.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,21 +7,21 @@ export const load = async (loadEvent) => {
 	let coords = {
 		latitude: 37.5087,
 		longitude: 126.7219,
-		accuracy: 42000
+		accuracy: 20_000_000
 	};
 
 	const city = headers.get('x-vercel-ip-city');
 	const country = headers.get('x-vercel-ip-country');
-	const latitude = headers.get('x-vercel-ip-latitude');
-	const longitude = headers.get('x-vercel-ip-longitude');
+	const latitude = Number(headers.get('x-vercel-ip-latitude'));
+	const longitude = Number(headers.get('x-vercel-ip-longitude'));
 
 	if (city && country && latitude && longitude) {
 		source = 'vercel-headers';
 		name = `${city}, ${country}`;
 		coords = {
-			latitude: 37.5087,
-			longitude: 126.7219,
-			accuracy: 42
+			latitude,
+			longitude,
+			accuracy: 25_000
 		};
 	}
 

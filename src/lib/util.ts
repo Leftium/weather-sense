71a1edf2d1354df6adad91bce8ecf03c36e60001
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-05-17 04:06:56 +0900

    Tweak WMO code colors and descriptions.
    Based on: https://gist.github.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -32,7 +32,7 @@ function wmoInterpretation(color: string, description: string, icon: string) {
 export const WMO_CODES: Record<number, any> = {
 	0: wmoInterpretation('#eeeef5', 'Clear', 'clear'),
 
-	1: wmoInterpretation('#d5dae2', 'Mainly Clear', 'mostly-clear'),
+	1: wmoInterpretation('#d5dae2', 'Mostly Clear', 'mostly-clear'),
 	2: wmoInterpretation('#b6bfcb', 'Partly Cloudy', 'partly-cloudy'),
 	3: wmoInterpretation('#878f9a', 'Overcast', 'overcast'),
 
@@ -40,36 +40,36 @@ export const WMO_CODES: Record<number, any> = {
 	48: wmoInterpretation('#878f9a', 'Icy Fog', 'rime-fog'),
 
 	51: wmoInterpretation('#80a5d6', 'Light Drizzle', 'light-drizzle'),
-	53: wmoInterpretation('#80a5d6', 'Moderate Drizzle', 'moderate-drizzle'),
+	53: wmoInterpretation('#80a5d6', 'Drizzle', 'moderate-drizzle'),
 	55: wmoInterpretation('#80a5d6', 'Heavy Drizzle', 'dense-drizzle'),
 
-	56: wmoInterpretation('#0F0', 'Light Freezing Drizzle', 'light-freezing-drizzle'),
-	57: wmoInterpretation('#0F0', 'Heavy Freezing Drizzle', 'dense-freezing-drizzle'),
+	56: wmoInterpretation('#80a5d6', 'Light Freezing Drizzle', 'light-freezing-drizzle'),
+	57: wmoInterpretation('#80a5d6', 'Freezing Drizzle', 'dense-freezing-drizzle'),
 
-	61: wmoInterpretation('#4a80c7', 'Slight Rain', 'light-rain'),
-	63: wmoInterpretation('#4a80c7', 'Moderate Rain', 'moderate-rain'),
+	61: wmoInterpretation('#4a80c7', 'Light Rain', 'light-rain'),
+	63: wmoInterpretation('#4a80c7', 'Rain', 'moderate-rain'),
 	65: wmoInterpretation('#4a80c7', 'Heavy Rain', 'heavy-rain'),
 
-	66: wmoInterpretation('#0F0', 'Light Freezing Rain', 'light-freezing-rain'),
-	67: wmoInterpretation('#0F0', 'Heavy Freezing Rain', 'heavy-freezing-rain'),
+	66: wmoInterpretation('#4a80c7', 'Light Freezing Rain', 'light-freezing-rain'),
+	67: wmoInterpretation('#4a80c7', 'Freezing Rain', 'heavy-freezing-rain'),
 
-	71: wmoInterpretation('#aba4db', 'Slight Snow', 'slight-snowfall'),
-	73: wmoInterpretation('#8c82ce', 'Moderate Snow', 'moderate-snowfall'),
+	71: wmoInterpretation('#aba4db', 'Light Snow', 'slight-snowfall'),
+	73: wmoInterpretation('#8c82ce', 'Snow', 'moderate-snowfall'),
 	75: wmoInterpretation('#8c82ce', 'Heavy Snow', 'heavy-snowfall'),
 
-	77: wmoInterpretation('#0F0', 'Snow Grains', 'snowflake'),
+	77: wmoInterpretation('#aba4db', 'Snow Grains', 'snowflake'),
 
-	80: wmoInterpretation('#0F0', 'Slight Rain Showers', 'light-rain'),
-	81: wmoInterpretation('#0F0', 'Moderate Rain Showers', 'moderate-rain'),
-	82: wmoInterpretation('#0F0', 'Heavy Rain Showers', 'heavy-rain'),
+	80: wmoInterpretation('#4a80c7', 'Light Showers', 'light-rain'),
+	81: wmoInterpretation('#4a80c7', 'Showers', 'moderate-rain'),
+	82: wmoInterpretation('#4a80c7', 'Heavy Showers', 'heavy-rain'),
 
-	85: wmoInterpretation('#0F0', 'Slight Snow Showers', 'slight-snowfall'),
-	86: wmoInterpretation('#0F0', 'Heavy Snow Showers', 'heavy-snowfall'),
+	85: wmoInterpretation('#aba4db', 'Light Snow Showers', 'slight-snowfall'),
+	86: wmoInterpretation('#8c82ce', 'Snow Showers', 'heavy-snowfall'),
 
-	95: wmoInterpretation('#0F0', 'Thunderstorm', 'thunderstorm'),
+	95: wmoInterpretation('#333333', 'Thunderstorm', 'thunderstorm'),
 
-	96: wmoInterpretation('#0F0', 'Thunderstorm with Light Hail', 'thunderstorm-with-hail'),
-	99: wmoInterpretation('#0F0', 'Thunderstorm with Heavy Hail', 'thunderstorm-with-hail')
+	96: wmoInterpretation('#333333', 'Light Thunderstorm w/ Hail', 'thunderstorm-with-hail'),
+	99: wmoInterpretation('#333333', 'Thunderstorm w/ Hail', 'thunderstorm-with-hail')
 };
 
 export function wmoCode(code: number | undefined) {

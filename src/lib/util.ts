e2070ee7c6129d9de9b58c28785eb3e30d5a3204
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-05-18 00:59:14 +0900

    Abbreviate WMO descriptions even more.

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -51,36 +51,36 @@ export const WMO_CODES: Record<number, any> = {
 	45: wmoInterpretation('#A4ACBA', 'Fog', 'fog'),
 	48: wmoInterpretation('#8891A4', 'Icy Fog', 'rime-fog'),
 
-	51: wmoInterpretation('#3DECEB', 'Lt Drizzle', 'light-drizzle'),
+	51: wmoInterpretation('#3DECEB', 'L.Drizzle', 'light-drizzle'),
 	53: wmoInterpretation('#0CCECE', 'Drizzle', 'moderate-drizzle'),
-	55: wmoInterpretation('#0AB1B1', 'Hv Drizzle', 'dense-drizzle'),
+	55: wmoInterpretation('#0AB1B1', 'H.Drizzle', 'dense-drizzle'),
 
-	80: wmoInterpretation('#9BCCFD', 'Lt Showers', 'light-rain'),
+	80: wmoInterpretation('#9BCCFD', 'L.Showers', 'light-rain'),
 	81: wmoInterpretation('#51B4FF', 'Showers', 'moderate-rain'),
-	82: wmoInterpretation('#029AE8', 'Hv Showers', 'heavy-rain'),
+	82: wmoInterpretation('#029AE8', 'H.Showers', 'heavy-rain'),
 
-	61: wmoInterpretation('#BFC3FA', 'Lt Rain', 'light-rain'),
+	61: wmoInterpretation('#BFC3FA', 'L.Rain', 'light-rain'),
 	63: wmoInterpretation('#9CA7FA', 'Rain', 'moderate-rain'),
-	65: wmoInterpretation('#748BF8', 'Hv Rain', 'heavy-rain'),
+	65: wmoInterpretation('#748BF8', 'H.Rain', 'heavy-rain'),
 
-	56: wmoInterpretation('#CAC1EE', 'Lt Icy Drizzle', 'light-freezing-drizzle'),
+	56: wmoInterpretation('#CAC1EE', 'L.Icy Drizzle', 'light-freezing-drizzle'),
 	57: wmoInterpretation('#9486E1', 'Icy Drizzle', 'dense-freezing-drizzle'),
 
-	66: wmoInterpretation('#D3BFE8', 'Lt Icy Rain', 'light-freezing-rain'),
+	66: wmoInterpretation('#D3BFE8', 'L.Icy Rain', 'light-freezing-rain'),
 	67: wmoInterpretation('#A780D4', 'Icy Rain', 'heavy-freezing-rain'),
 
-	71: wmoInterpretation('#F9B1D8', 'Lt Snow', 'slight-snowfall'),
+	71: wmoInterpretation('#F9B1D8', 'L.Snow', 'slight-snowfall'),
 	73: wmoInterpretation('#F983C7', 'Snow', 'moderate-snowfall'),
-	75: wmoInterpretation('#F748B7', 'Hv Snow', 'heavy-snowfall'),
+	75: wmoInterpretation('#F748B7', 'H.Snow', 'heavy-snowfall'),
 
 	77: wmoInterpretation('#E7B6EE', 'Snow Grains', 'snowflake'),
 
-	85: wmoInterpretation('#E7B6EE', 'Lt Snow Showers', 'slight-snowfall'),
+	85: wmoInterpretation('#E7B6EE', 'L.Snow Showers', 'slight-snowfall'),
 	86: wmoInterpretation('#CD68E0', 'Snow Showers', 'heavy-snowfall'),
 
 	95: wmoInterpretation('#B21E4F', 'Thunderstorm', 'thunderstorm'),
 
-	96: wmoInterpretation('#88143B', 'T-storm w/ Lt Hail', 'thunderstorm-with-hail'),
+	96: wmoInterpretation('#88143B', 'T-storm w/ L.Hail', 'thunderstorm-with-hail'),
 	99: wmoInterpretation('#5F0E28', 'T-storm w/ Hail', 'thunderstorm-with-hail')
 };
 

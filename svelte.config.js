3f60ff9e94c911b88401de4047cd5eb8c6eb4251
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-06-23 01:16:38 +0900

    Enable Svelte Inspector.

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,15 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+	},
+
+	vitePlugin: {
+		// set to true for defaults or customize with object
+		inspector: {
+			toggleKeyCombo: 'control-shift',
+			showToggleButton: 'always',
+			toggleButtonPos: 'bottom-right'
+		}
 	}
 };
 

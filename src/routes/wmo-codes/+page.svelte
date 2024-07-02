b3b3a7eee9dd627a2c8f59f1d6854654878c7f1e
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-07-02 11:15:44 +0900

    Prepare for new version of wmo-codes route.

--- src/routes/wmo-codes/+page.svelte
+++ src/routes/wmo-codes/+page.svelte
@@ -31,100 +31,104 @@
 	);
 </script>
 
-<div class="pico container">
-	{#each wmoCodesByType as wmoCodeType}
-		<div class="code-group flex space-between">
-			{#each wmoCodeType as data}
-				<article class="code flex flex-column align-center">
-					<div>
-						<span class="code transparent" hidden={data.code > 9}>0</span><span class="code"
-							>{data.code}</span
-						>
-						<img src={data.icon} alt="" />
-					</div>
-					<div class="description">
-						<div class:is-dark-text={data.isDarkText} style:background-color={data.color}>
-							{data.description}
-						</div>
-						<div class:is-dark-text={data.isDarkText} style:background-color={data.color}>
+<div class="orig">
+	<div class="pico container">
+		{#each wmoCodesByType as wmoCodeType}
+			<div class="code-group flex space-between">
+				{#each wmoCodeType as data}
+					<article class="code flex flex-column align-center">
+						<div>
+							<span class="code transparent" hidden={data.code > 9}>0</span><span class="code"
+								>{data.code}</span
+							>
 							<img src={data.icon} alt="" />
-							{data.color}
 						</div>
-					</div>
-				</article>
-			{/each}
-		</div>
-	{/each}
+						<div class="description">
+							<div class:is-dark-text={data.isDarkText} style:background-color={data.color}>
+								{data.description}
+							</div>
+							<div class:is-dark-text={data.isDarkText} style:background-color={data.color}>
+								<img src={data.icon} alt="" />
+								{data.color}
+							</div>
+						</div>
+					</article>
+				{/each}
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-	.container {
-		padding: 0.1em;
-	}
+	.orig {
+		.container {
+			padding: 0.1em;
+		}
 
-	.flex {
-		display: flex;
-		flex-wrap: wrap;
-	}
+		.flex {
+			display: flex;
+			flex-wrap: wrap;
+		}
 
-	.space-between {
-		justify-content: space-between;
-	}
+		.space-between {
+			justify-content: space-between;
+		}
 
-	.align-center {
-		align-items: center;
-	}
+		.align-center {
+			align-items: center;
+		}
 
-	.flex-column {
-		flex-direction: column;
-	}
+		.flex-column {
+			flex-direction: column;
+		}
 
-	.code {
-		font-size: larger;
-		font-weight: 900;
-		vertical-align: bottom;
-	}
+		.code {
+			font-size: larger;
+			font-weight: 900;
+			vertical-align: bottom;
+		}
 
-	.transparent {
-		opacity: 0;
-	}
+		.transparent {
+			opacity: 0;
+		}
 
-	.description {
-		font-size: small;
+		.description {
+			font-size: small;
 
-		overflow: hidden;
-		white-space: nowrap;
+			overflow: hidden;
+			white-space: nowrap;
 
-		text-overflow: ellipsis;
-	}
+			text-overflow: ellipsis;
+		}
 
-	.description img {
-		height: 22px;
-		width: 22px;
-	}
+		.description img {
+			height: 22px;
+			width: 22px;
+		}
 
-	.description div {
-		padding: 0 1em;
-		margin: 0.4em;
-		border-radius: 0.3em;
-		line-height: 40px;
-		font-size: 13px;
-		font-weight: 400;
+		.description div {
+			padding: 0 1em;
+			margin: 0.4em;
+			border-radius: 0.3em;
+			line-height: 40px;
+			font-size: 13px;
+			font-weight: 400;
 
-		color: #fff;
-		text-shadow: 1px 0 rgb(0 0 0 / 50%);
+			color: #fff;
+			text-shadow: 1px 0 rgb(0 0 0 / 50%);
 
-		text-align: center;
-	}
+			text-align: center;
+		}
 
-	.description .is-dark-text {
-		color: #333;
-		text-shadow: 1px 0 rgb(255 255 255 / 50%);
-	}
+		.description .is-dark-text {
+			color: #333;
+			text-shadow: 1px 0 rgb(255 255 255 / 50%);
+		}
 
-	.code-group > .code {
-		width: calc(97% / 3);
-		margin: 0.5%;
-		padding: 0.2em;
+		.code-group > .code {
+			width: calc(97% / 3);
+			margin: 0.5%;
+			padding: 0.2em;
+		}
 	}
 </style>

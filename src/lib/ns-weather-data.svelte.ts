d913245a80a4fd5eec388257937d657487f2bc6f
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-06-09 14:59:57 -0500

    Show more forecast days.

--- src/lib/ns-weather-data.svelte.ts
+++ src/lib/ns-weather-data.svelte.ts
@@ -40,7 +40,7 @@ export type WeatherDataEvents = {
 
 const DATEFORMAT_MASK = 'MM-DD hh:mma z';
 const PAST_DAYS = dev ? 2 : 1; // 0 to 92
-const FORECAST_DAYS = dev ? 4 : 5; // 0 to 16 for forecast; 0 to 7 for air-quality;
+const FORECAST_DAYS = dev ? 4 : 9; // 0 to 16 for forecast; 0 to 7 for air-quality;
 
 const { on, emit } = getEmitter<WeatherDataEvents>(import.meta);
 

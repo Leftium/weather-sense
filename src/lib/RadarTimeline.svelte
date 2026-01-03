a4059d5556078491497b3907ee62af6fc746e124
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-01-03 14:28:34 +0900

    style: redesign radar timeline slider for compact layout
    - Custom styled range slider with gray track and rounded rect thumb
    - Minimize vertical space with tighter spacing
    - Match play button color to slider thumb (#666)
    - Reduce play button size
    - Reset tick label styles for proper alignment
    - Add global sans-serif font family

--- src/lib/RadarTimeline.svelte
+++ src/lib/RadarTimeline.svelte
@@ -70,9 +70,9 @@
 	<div role="none" class="play-pause" {onclick}>
 		<iconify-icon
 			icon={`solar:${nsWeatherData.radarPlaying ? 'pause' : 'play'}-bold`}
-			width="1.5em"
-			height="1.5em"
-			style="color: black"
+			width="1em"
+			height="1em"
+			style="color: #666"
 		></iconify-icon>
 	</div>
 </div>
@@ -80,43 +80,85 @@
 <style>
 	.radar-timeline {
 		display: flex;
-		align-items: center;
-		padding: 0.125em;
-		height: 2.5em;
-
+		align-items: flex-start;
+		padding: 1px 0.5em 2px;
+		gap: 0.5em;
 		background-color: whitesmoke;
 	}
 
 	.play-pause {
-		margin-right: 6px;
-		margin-top: 8px;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		padding-top: 1px;
 	}
 
 	.range-wrapper {
 		flex-grow: 1;
+		line-height: 0;
 	}
 
-	input {
+	/* Custom range slider */
+	input[type='range'] {
+		-webkit-appearance: none;
+		appearance: none;
 		width: 100%;
-		padding: 0 calc(((100% / 17) - (23.75px)) / 2) !important;
-		margin: 0 !important;
+		height: 6px;
+		margin: 0;
+		padding: 0;
+		cursor: pointer;
+		background: #ccc;
+		border-radius: 3px;
+		display: block;
+	}
+
+	/* Thumb - webkit */
+	input[type='range']::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		appearance: none;
+		width: 16px;
+		height: 10px;
+		background: #666;
+		border-radius: 2px;
+		border: none;
+		box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+		transform: translateY(2px);
+	}
+
+	/* Thumb - Firefox */
+	input[type='range']::-moz-range-thumb {
+		width: 16px;
+		height: 10px;
+		background: #666;
+		border-radius: 2px;
+		border: none;
+		box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+	}
+
+	/* Track - Firefox */
+	input[type='range']::-moz-range-track {
+		background: #ccc;
+		height: 6px;
+		border-radius: 3px;
 	}
 
 	datalist#radar-markers {
 		width: 100%;
 		display: flex;
 		justify-content: space-between;
+		margin: 4px 0 0 0;
+		padding: 0;
 	}
 
 	div.tick {
-		width: calc(100% / 17);
+		margin: 0;
+		padding: 0;
 		opacity: 0;
-
-		font-size: x-small;
+		font-size: 9px;
+		line-height: 1;
 		text-align: center;
 		text-wrap: nowrap;
-
-		color: black;
+		color: #666;
 	}
 
 	div.loaded {
@@ -130,10 +172,6 @@
 	@media (max-width: 510px) {
 		div.tick.minor-time {
 			visibility: hidden;
-		}
-
-		.play-pause {
-			margin-left: 8px;
 		}
 	}
 </style>

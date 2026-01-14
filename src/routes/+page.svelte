9bceb1d852c32b13e95c143c931d64e194ab7ffa
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-01-14 15:23:02 +0900

    debug: log showMinutely to console

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -79,6 +79,14 @@
 
 	// Show minutely plot when ?m param is present in URL
 	const showMinutely = $derived(browser && $page.url.searchParams.has('m'));
+	$effect(() => {
+		console.log('DEBUG showMinutely:', {
+			showMinutely,
+			browser,
+			hasM: $page.url.searchParams.has('m'),
+			url: $page.url.href,
+		});
+	});
 
 	// URL with ?m param added (preserves other params like location)
 	const minutelyUrl = $derived.by(() => {

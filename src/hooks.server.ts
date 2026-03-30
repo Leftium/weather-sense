f8b9c3cdda28b22f4fb53615cb5baf72a30a795c
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-03-30 14:04:07 +0900

    chore: format.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,3 @@
-import { createHandler } from 'web-sentinel/hooks'
+import { createHandler } from 'web-sentinel/hooks';
 
-export const handle = createHandler({ log: true, preview: false, http_status: 418 })
+export const handle = createHandler({ log: true, preview: false, http_status: 418 });

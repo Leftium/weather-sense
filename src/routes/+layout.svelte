b1d0acd744669dac6c04f54f6b191b19e0c22e5b
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2026-04-01 17:58:11 +0900

    chore: replace @leftium/logo favicon with static logo pack

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-	import { favicon } from '@leftium/logo';
 	import { GgConsole } from '@leftium/gg';
 	import '../app.scss';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<title>WeatherSense</title>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <GgConsole />
 {@render children?.()}
